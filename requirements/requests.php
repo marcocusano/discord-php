85fdfa7af684198d0207881173e783020d1527cb
Marco Cusano <37231387+marcocusano@users.noreply.github.com>
2019-05-11 06:53:05 +0200

    Syntax fix
    - Fixed missing global var
    - Fixed syntax error

--- requirements/requests.php
+++ requirements/requests.php
@@ -3,7 +3,7 @@
     class DISCORD_REQUESTS {
 
         function custom($url, $method = "PUT", $params = null, $content_length = null, $authType = "Bot", $authKey = "token", $results = 0) { global $discord;
-            return $discord->requests->requestor($url, $method, $params, $content_length, $authType $authKey, $results);
+            return $discord->requests->requestor($url, $method, $params, $content_length, $authType, $authKey, $results);
         }
 
         function delete($endpoint) { global $discord;
