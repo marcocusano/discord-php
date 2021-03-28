1e7972cff2163287f1d000adaf016a2f13ce3d1a
Marco Cusano <37231387+marcocusano@users.noreply.github.com>
2021-03-28 17:28:20 +0200

    Delete requirements directory

--- requirements/custom.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    // Add here your PHP Custom Code.
-    // You can also create files in Custom Folder adding here the following code to import : require_once(__DISCORD_CUSTOM__ . "/your_file_name.php");
-    // You can also create json files inside the Custom Folder adding here the following code : $your_var = json_decode(file_get_contents(__DISCORD_CUSTOM__ . "/your_file_name.json"));
-
-?>
